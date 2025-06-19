79f12100baa67ef623adad8d10e10b925b091be7
saifurrehman065 <saif.ciit@gmail.com>
2025-06-19 17:47:17 +0500

    Update Attendance.sql

--- dbo/Tables/Attendance.sql
+++ dbo/Tables/Attendance.sql
@@ -3,6 +3,7 @@
     [EmployeeID]     INT           NULL,
     [AttendanceDate] DATE          NULL,
     [Status]         NVARCHAR (20) NULL,
+    [CreatedOn] DATE NULL, 
     PRIMARY KEY CLUSTERED ([AttendanceID] ASC),
     FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees] ([EmployeeID])
 );
