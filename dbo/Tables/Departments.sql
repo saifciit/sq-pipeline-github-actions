575356b375d3b77343599acc79ca95bfdca25ef5
saifurrehman065 <saif.ciit@gmail.com>
2025-06-20 20:33:18 +0500

    Update Departments.sql

--- dbo/Tables/Departments.sql
+++ dbo/Tables/Departments.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Departments] (
     [DepartmentID]   INT            IDENTITY (1, 1) NOT NULL,
     [DepartmentName] NVARCHAR (100) NOT NULL,
+    [Title] NCHAR(10) NULL, 
+    [CreatedDate] DATE NULL, 
     PRIMARY KEY CLUSTERED ([DepartmentID] ASC)
 );
 
